27c293887f008d57452d3e5e80190c4b0adf68de
Aachal121-code <aachalbhonde19@gmail.com>
2025-12-02 15:27:23 +0530

    make login page more format

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="assets//css//login.css" type="text/css">
     <title>ReMindMe - Login</title>
 </head>
 
@@ -15,18 +16,24 @@
 
     <h1>🔐 Login to Continue</h1>
 
-    <form action="authentication.php" method="post">
-
-        <label for="email">Email ID:</label>
-        <input type="text" id="email" name="email" required>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-
-        <button type="submit">Login</button>
-
-        <a href="register.php">Don't have an account?</a>
-    </form>
+    <div class="login-container">
+        <form action="authentication.php" method="post">
+            
+            <div id="email-input">
+                <label for="email">Email ID:</label><br>
+                <input type="text" id="email" name="email" required><br>
+            </div>
+            
+            <div id="password-input">
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" required><br>
+            </div>
+            
+            <button type="submit">Login</button>
+            
+            <a href="register.php">Don't have an account?</a>
+        </form>
+    </div>
 
 </body>
 </html>
