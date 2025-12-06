9d7a01ccc9a7865d6ce55240526913e6b20ce359
Aachal121-code <aachalbhonde19@gmail.com>
2025-12-06 14:53:12 +0530

    make register page connect to login

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="assets//css//register.css" type="text/css">
+    <title>ReMindMe - register</title>
+</head>
+
+<body>
+
+    <div class="back">
+        <i class="fa-solid fa-arrow-left"></i>
+    </div>
+
+    <h1>Create Your Account</h1>
+
+    
+
+</body>
+</html>
