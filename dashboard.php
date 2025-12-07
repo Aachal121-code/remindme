1813b03078f114f4cd50b3679fb117140c473b7d
Aachal121-code <aachalbhonde19@gmail.com>
2025-12-07 13:56:19 +0530

    add cdn link and link css file to dashboard

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="assets//css//dashboard.css" type="text/css">
     <title>ReMindMe - dashboard</title>
 </head>
 <body>
@@ -47,17 +49,10 @@
                 <div class="documents" id="documentList">
                     <p>No documents added yet.</p>
                 </div>
-            </div>
+            </div>    
 
         </div>
     </section>
         
-
-
-
-
-
-
-
 </body>
 </html>
