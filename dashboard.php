b9f8c616fb0fd25b30715bdd12d8619f789fee5e
Aachal121-code <aachalbhonde19@gmail.com>
2025-12-07 13:45:47 +0530

    add navbar options on dashboard

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ReMindMe - dashboard</title>
+</head>
+<body>
+    <section class="dashboard">
+        <div class="navbar">
+            <div class="logo">
+                <p>ReMindMe</p>
+            </div>
+            <div class="addDocument">
+                <button id="addDocumentBtn">+ Add Document</button>
+            </div>
+            <div class="setting">
+                <button id="settingBtn">⚙️</button>
+            </div>
+        </div>
+        
+    </section>
+        
+
+
+
+
+
+
+
+</body>
+</html>
