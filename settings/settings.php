314410ab56da2bab078f6032f03bf4ee61f5a709
Aachal121-code <aachalbhonde19@gmail.com>
2026-01-02 15:39:08 +0530

    link proper css file

--- settings/settings.php
+++ settings/settings.php
@@ -15,7 +15,7 @@ $page = $_GET['page'] ?? 'menu';
     <meta charset="UTF-8">
     <title>Settings - ReMindMe</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/setting.css">
+    <link rel="stylesheet" href="../assets/css/Setting.css">
 </head>
 <body class="<?= $page==='menu' ? 'menu-page' : '' ?>">
 
