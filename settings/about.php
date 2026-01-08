85bf9b0ce2456e93e041f8d1579f08601bcb4fed
Aachal121-code <aachalbhonde19@gmail.com>
2026-01-08 14:20:42 +0530

    change about app data

--- settings/about.php
+++ settings/about.php
@@ -5,7 +5,7 @@
 <h3>About ReMindMe</h3>
 <div class="card">
     <p><strong>Version:</strong> 1.0</p>
-    <p><strong>Developer:</strong> Your Name</p>
-    <p>ReMindMe is a simple tool that helps you track expiry dates of your important documents and receive reminders.</p>
-    <p>Contact: remindme.help@gmail.com</p>
+    <p><strong>Developer:</strong> Aachal Bhonde</p>
+    <p>RemindMe is a simple tool that helps you track expiry dates of your important documents and receive reminders.</p>
+    <p>Contact: remindme.alert@gmail.com</p>
 </div>
