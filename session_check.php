d6d7128921aa0568978d7948a56425646cf3e546
Aachal121-code <aachalbhonde19@gmail.com>
2026-02-03 15:34:37 +0530

    make one file for session checking

---
+++ session_check.php
@@ -0,0 +1,21 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$timeout = 30 * 60; // 30 minutes
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
+    session_unset();
+    session_destroy();
+    header('Location: login.php?timeout=1');
+    exit;
+}
+
+$_SESSION['last_activity'] = time();
+?>
